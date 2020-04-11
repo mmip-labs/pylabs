10f5ee5322a55692726a30881b5f993659581bdc
Alex Smith <kappa_test@mail.ru>
2020-04-11 18:29:31 +0300

    New commit 11.04

--- 5_strings.py
+++ 5_strings.py
@@ -9,13 +9,14 @@ rank = 400
 
 s = 'Name: {movie}\nGenre: {gen}.\nRank - {rnk}'
 
-#result2 = 'Name: ' + name + ' ' + genre
+result2 = 'Name: ' + name + ' ' + genre
 
-#result = s.format(movie=name,gen=genre,rnk=rank)
+result = s.format(movie=name,gen=genre,rnk=rank)
 
 result = f'Movie: {name}. Genre: {genre}. Rank: {rank}'
 
 
 print(result)
 
-#print(result2)
+print(result2)
+
